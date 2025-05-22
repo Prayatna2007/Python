c0da15adec058d035f21e909968500ec031f8d3a
prayatna <shresthaprayatna1@gmail.com>
2025-05-22 22:20:45 +0545

    Just Format

--- str_operations.py
+++ str_operations.py
@@ -18,16 +18,16 @@ def rev():
         reversed_word += x[i]
     print(reversed_word)
 
-while(1):
+while(4):
     print("Enter :\n 1. For Checking Palindrome\n 2. For Reversing String\n 3. For Exit\n\n")
-    x=int(input("Enter acc to your choice (1-2) :"))
+    x=int(input("Enter acc to your choice (1-3) :"))
     match(x):
         case 1: 
             palindrome()
         case 2:
             rev()
         case 3:
-            exit(1)
+            exit(4)
 
 
     
