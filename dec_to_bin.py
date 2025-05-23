67cd8f8b9275e32f8ab713cb8a8eec9a21893c72
prayatna <shresthaprayatna1@gmail.com>
2025-05-23 07:48:40 +0545

    Binary to Decimal And Decimal to Binary Converter

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -10,4 +10,17 @@ def dec_to_bin():
         i=i+1
     print(f"The Decimal value is {a} and Binary value is {s}")
 
+def bin_to_dec():
+    n=int(input("Enter any binary number:"))
+    a=n
+    i=0
+    s=0
+    while(n>0):
+        r = n%10
+        s += r*pow(2,i)
+        n=n//10
+        i=i+1
+    print(f"The Binary value is {a} and Decimal Value is {s}")
+
+bin_to_dec()
 dec_to_bin()
