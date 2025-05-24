133ad08d8a748c59117b6dd5ecc81be9f36ee662
prayatna <shresthaprayatna1@gmail.com>
2025-05-24 19:27:29 +0545

    Number Guessing Game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -3,19 +3,25 @@ import random
 def number_guessing_game():
     secret_number = random.randint(1, 100)
     attempts = 0
+    attempts_left=10
     
     print("\n\n_____________Welcome to the Number Guessing Game!___________\n")
     print("_________I'm thinking of a number between 1 and 100.________\n")
     
-    while True:
+    while (1):
         try:
             # Takes player's guess
             print("\n")
+            print(f"You have total {attempts_left} attempts left")
             guess = int(input("\t\tTake a guess: "))
             attempts += 1
+            attempts_left=attempts_left-1
             
             # For checking the correct number
-            if guess < secret_number:
+            if attempts_left==0:
+                print("You lost .....")
+                exit(1)
+            elif guess < secret_number:
                 print("\t\tLow! Try again.")
             elif guess > secret_number:
                 print("\t\tHigh! Try again.")
