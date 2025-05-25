e8169150e16fa6cd4e614e3510e3dc48828b67a6
prayatna <shresthaprayatna1@gmail.com>
2025-05-25 23:53:28 +0545

    Number Guessing Game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -3,31 +3,33 @@ import random
 def number_guessing_game():
     secret_number = random.randint(1, 100)
     attempts = 0
-    attempts_left=10
+    attempts_left = 10
     
     print("\n\n_____________Welcome to the Number Guessing Game!___________\n")
     print("_________I'm thinking of a number between 1 and 100.________\n")
     
-    while (1):
+    while True:
         try:
-            # Takes player's guess
-            print("\n")
-            print(f"You have total {attempts_left} attempts left")
+            print(f"\nYou have {attempts_left} attempts left")
             guess = int(input("\t\tTake a guess: "))
             attempts += 1
-            attempts_left=attempts_left-1
+            attempts_left -= 1
             
-            # For checking the correct number
-            if attempts_left==0:
-                print("You lost .....")
-                exit(1)
-            elif guess < secret_number:
-                print("\t\tLow! Try again.")
-            elif guess > secret_number:
-                print("\t\tHigh! Try again.")
-            else:
+            if attempts_left == 0 and guess != secret_number:
+                print("\t\tYou lost...")
+                print("\t\tThe secret number was", secret_number)
+                break
+            elif guess == secret_number:
                 print(f"\t\tCongratulations! You guessed the number in {attempts} attempts!")
                 break
+            elif guess < secret_number - 20:
+                print("\t\tVery Low! Try again.")
+            elif guess < secret_number:
+                print("\t\tLow! Try again.")
+            elif guess > secret_number + 20:
+                print("\t\tVery High! Try again.")
+            elif guess > secret_number:
+                print("\t\tHigh! Try again.")
         except ValueError:
             print("\t\tPlease enter a valid number.")
 
