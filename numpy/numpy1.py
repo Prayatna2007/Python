59f986218ace96cbc84741d94bcbeb59655e2f3a
prayatna <shresthaprayatna1@gmail.com>
2025-06-14 22:32:26 +0545

    Basic Numpy Program

---
+++ numpy/numpy1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Creating a NumPy array
+arr = np.array([1, 2, 3, 4, 5])
+
+print("Original array:", arr)
+print("Add 5 to each element:", arr + 5)
+print("Multiply each element by 2:", arr * 2)
+print("Sum of all elements:", np.sum(arr))
+print("Mean of all elements:", np.mean(arr))
